c8735fcad6773bb26f5bd668512582db2a06182c
meshekhar <meshekhar@gmail.com>
2020-07-21 14:29:17 -0500

    Updates

---
+++ precache-manifest.724ce583ba47af00e0b5b422b13a8cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8aae5525ed23aad119fa85bf508f3b5",
+    "url": "/kantito/index.html"
+  },
+  {
+    "revision": "f772b562ea79bac431dd",
+    "url": "/kantito/static/css/main.f5c2190c.chunk.css"
+  },
+  {
+    "revision": "5da6bb7439c06051c2e3",
+    "url": "/kantito/static/js/2.0cbdb77f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kantito/static/js/2.0cbdb77f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f772b562ea79bac431dd",
+    "url": "/kantito/static/js/main.3ae22e0b.chunk.js"
+  },
+  {
+    "revision": "f754573327ebf754d6d5",
+    "url": "/kantito/static/js/runtime-main.1cbd1162.js"
+  }
+]);
